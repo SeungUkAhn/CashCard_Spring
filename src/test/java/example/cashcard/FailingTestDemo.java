42d9a722dd24fdfc6ecada26c23b1ecf98c6ade5
SeungUk Ahn <ahn2802@gmail.com>
2023-04-28 18:37:29 +0900

    실패하는 테스트 만들어보기

---
+++ src/test/java/example/cashcard/FailingTestDemo.java
@@ -0,0 +1,11 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FailingTestDemo {
+    //@Test
+    public void myFirstTest(){
+        assertThat(1).isEqualTo(42);
+    }
+}
