57adc238de7e3908dd43f17f8f6d9c51d4c9073a
SeungUk Ahn <ahn2802@gmail.com>
2023-04-29 20:50:48 +0900

    CashCardApplicationTests 작성 (GET)
    - 실패 케이스 추가

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -1,13 +1,23 @@
 package example.cashcard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //스프링부트 앱 실행 후 테스트에서 앱으로 요청을 보낼 수 있도록 해주는 어노테이션
 class CashCardApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
 
     @Test
-    void contextLoads() {
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK); //응답코드 200
     }
 
 }
