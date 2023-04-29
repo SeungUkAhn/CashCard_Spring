6a825719f282baad68ad9e052bdaea342202ce5d
SeungUk Ahn <ahn2802@gmail.com>
2023-04-30 03:06:58 +0900

    CashCardController 수정
    - CashCardRepository 추가
    - findById 메서드 수정: repository를 거쳐 DB에서 ID에 해당하는 CashCard를 조회하도록 수정

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -6,15 +6,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+    private CashCardRepository cashCardRepository;
+    public CashCardController(CashCardRepository cashCardRepository){   //생성자(별도의 생성 코드 없이도 실행 시 스프링이 처리)
+        this.cashCardRepository = cashCardRepository;
+    }
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        //id값 99일때만 통과시키기
-        if(requestedId.equals(99L)) {
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId); //Optional 객체 생성 후 ID로 CashCard 찾기
+        if(cashCardOptional.isPresent()) {  //해당 ID가 존재하는 경우
             CashCard cashCard = new CashCard(requestedId, 0.0);
-            return ResponseEntity.ok(cashCard);
+            return ResponseEntity.ok(cashCardOptional.get());
         }else{
             return ResponseEntity.notFound().build();
         }
