bdb011390d0e902cbc7a30b0ce19dfe5e63ee4a4
SeungUk Ahn <ahn2802@gmail.com>
2023-04-30 01:00:49 +0900

    테스트케이스 추가에 따른 컨트롤러 수정
    - PathVariable에 따라 테스트 통과여부 결정하도록 변경

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -26,4 +26,11 @@ class CashCardApplicationTests {
         assertThat(id).isEqualTo(99);
     }
 
+    @Test
+    void shouldNotReturnACashCardWithAnUnknownId() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isBlank();
+    }
+
 }
